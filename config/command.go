53ec8a8426aeaeca99d8ef2de1df2bae2480495d
Joseph Hollinshead <jhollinshead@hotels.com>
2018-07-02 17:05:15 +0100

    Fix PUT

--- config/command.go
+++ config/command.go
@@ -24,57 +24,14 @@ func newHandlerAndEvents(cfg Command, envs map[string]string) (func(input json.R
 	cd.successEvent = flyte.EventDef{Name: fmt.Sprintf("%sSuccess", cfg.Name)}
 	cd.failureEvent = flyte.EventDef{Name: fmt.Sprintf("%sFailure", cfg.Name)}
 
-	switch strings.ToUpper(cd.cfg.Request.Type) {
-	case http.MethodPost:
-		handler, err := cd.createPostHandler()
-		if err != nil {
-			return nil, []flyte.EventDef{}, err
-		}
-		return handler, []flyte.EventDef{cd.successEvent, cd.failureEvent}, nil
-
-	case http.MethodGet:
-		handler, err := cd.createGetHandler()
-		if err != nil {
-			return nil, []flyte.EventDef{}, err
-		}
-		return handler, []flyte.EventDef{cd.successEvent, cd.failureEvent}, nil
+	handler, err := cd.createHandler()
+	if err != nil {
+		return nil, []flyte.EventDef{}, err
 	}
-
-	return nil, []flyte.EventDef{}, errors.New(fmt.Sprintf("unknown request type '%s', ", cd.cfg.Request.Type))
+	return handler, []flyte.EventDef{cd.successEvent, cd.failureEvent}, nil
 }
 
-func (cd *command) createGetHandler() (func(input json.RawMessage) flyte.Event, error) {
-
-	return func(input json.RawMessage) flyte.Event {
-
-		resolvedInputs, err := cd.resolveInputs(input)
-		if err != nil {
-			logger.Error(err)
-			return flyte.Event{EventDef: cd.failureEvent, Payload: err}
-		}
-
-		// Inject variable values in path
-		path := injectVars(cd.cfg.Request.Path, resolvedInputs, cd.envs)
-
-		r, err := http.NewRequest(http.MethodGet, path, nil)
-		if err != nil {
-			logger.Error(err)
-			return flyte.Event{EventDef: cd.failureEvent, Payload: err}
-		}
-		cd.setHeaders(r)
-
-		payload, err := sendRequest(r)
-		if err != nil {
-			logger.Error(err)
-			return flyte.Event{EventDef: cd.failureEvent, Payload: err}
-		}
-
-		logger.Debug(fmt.Sprintf("successful event, payload: %+v", payload))
-		return flyte.Event{EventDef: cd.successEvent, Payload: payload}
-	}, nil
-}
-
-func (cd *command) createPostHandler() (func(input json.RawMessage) flyte.Event, error) {
+func (cd *command) createHandler() (func(input json.RawMessage) flyte.Event, error) {
 
 	return func(input json.RawMessage) flyte.Event {
 
@@ -86,9 +43,8 @@ func (cd *command) createPostHandler() (func(input json.RawMessage) flyte.Event,
 
 		// Inject variable values in path and data
 		path := injectVars(cd.cfg.Request.Path, resolvedInputs, cd.envs)
-		data := injectVars(cd.cfg.Request.Data, resolvedInputs, cd.envs)
 
-		r, err := http.NewRequest(http.MethodPost, path, bytes.NewBuffer([]byte(data)))
+		r, err := cd.createRequest(resolvedInputs, path)
 		if err != nil {
 			logger.Error(err)
 			return flyte.Event{EventDef: cd.failureEvent, Payload: err}
@@ -104,6 +60,23 @@ func (cd *command) createPostHandler() (func(input json.RawMessage) flyte.Event,
 		logger.Debug(fmt.Sprintf("successful event, payload: %+v", payload))
 		return flyte.Event{EventDef: cd.successEvent, Payload: payload}
 	}, nil
+}
+
+func (cd *command) createRequest(resolvedInputs map[string]string, path string) (*http.Request, error) {
+	if cd.cfg.Request.Type == http.MethodPost || cd.cfg.Request.Type == http.MethodPut {
+		data := injectVars(cd.cfg.Request.Data, resolvedInputs, cd.envs)
+		r, err := http.NewRequest(cd.cfg.Request.Type, path, bytes.NewBuffer([]byte(data)))
+		if err != nil {
+			return nil, err
+		}
+		return r, nil
+	}
+
+	r, err := http.NewRequest(cd.cfg.Request.Type, path, nil)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
 }
 
 func (cd *command) resolveInputs(input json.RawMessage) (map[string]string, error) {
